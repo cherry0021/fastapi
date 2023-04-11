8f172d4f6497de6a38a6bb8e7ced2ef2135c97fc
gello mendez <128371781+cherry0021@users.noreply.github.com>
2023-04-11 12:14:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -166,7 +166,7 @@ async def daxko1_api(client_info):
         card_type = "discover"
     
     configs = {
-        'CHROME_BUNDLE': "app/gates/utils/chromedriver.exe",
+        'CHROME_BUNDLE': "chromedriver.exe",
         'HEADLESS': 'true'
         }
     
